15ac71274641b8014fcacf8165551ed79c5424b5
Juergen Edelbluth <juergen@edelbluth.eu>
2016-10-10 14:27:15 +0200

    Signing and Coverage in Profiles

--- deploy-release.sh
+++ deploy-release.sh
@@ -4,5 +4,5 @@ version_append=$(date +%Y%m%d%H%M%S)
 meta_append=$(git log -1 --no-decorate --format=%h)
 
 mvn build-helper:parse-version versions:set -DnewVersion=\${parsedVersion.majorVersion}.\${parsedVersion.minorVersion}-${version_append}+${meta_append} -DgenerateBackupPoms=true
-mvn clean deploy
+mvn clean deploy -Psign
 mvn versions:revert
